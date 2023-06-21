dd5cecd185fc913662f1f0f459b54e71427905f9
OpenPrattay <orginalprattaysarkar@gmail.com>
2023-06-21 15:31:14 +0530

    Y/N System Removed And Colorama Colors Remaining After Errors Or Force Exit Is Handled With Error Handling!

--- main.py
+++ main.py
@@ -17,18 +17,17 @@ def continueprcs():
     try:
         ser = serial.Serial(comport, baudrate)
     except KeyboardInterrupt:
-        print(Fore.RED + "Something Has Gone Really Wrong! Check Details Please!\nClosing Program...")
+        sys.exit(0)
+    except serial.SerialException:
+        print(Fore.RED + "Details Are Wrong, Please Check!" + Style.RESET_ALL)
+    except:
+        print(Fore.RED + "Something Has Gone Really Wrong!\nClosing Program..." + Style.RESET_ALL)
         sys.exit(2)
     print(Fore.GREEN + "Connected To Serial Port!" + Style.RESET_ALL)
-
-    tosendorrecv = input(Fore.BLUE + "Do You Want To Send or Receive Data? (s/r): ")
-    if str(tosendorrecv.lower()) == "s":
-        datatobesent = input(Fore.BLUE + "What Do You Want To Send?: ")
-        if len(str(datatobesent)) == 0:
-            print(Fore.RED + "Data Can't Be Empty!" + Style.RESET_ALL)
-            exit(1)
-    elif str(tosendorrecv.lower()) == "":
-        print(Fore.GREEN + "Received Data: " + str(ser.readline()))
+    datatobesent = input(Fore.BLUE + "What Do You Want To Send?: ")
+    if len(str(datatobesent)) == 0:
+        print(Fore.RED + "Data Can't Be Empty!" + Style.RESET_ALL)
+        exit(1)
 
 
 # Check if arguments are missing
@@ -42,16 +41,24 @@ baudrate = sys.argv[2]
 
 # Check if any argument is empty
 if not comport or not baudrate:
-    print(Fore.RED + "Error: Both arguments are required.\nUsage: python script.py <COM Port> <Baud Rate>")
+    print(Fore.RED + "Error: Both arguments are required.\nUsage: python script.py <COM Port> <Baud Rate>" + Style.RESET_ALL)
     sys.exit(1)
 
 # Use the arguments
 print(Fore.GREEN + "COM PORT Given: " + comport)
 print(Fore.GREEN + "BAUD RATE Given: " + baudrate)
-
-ynconfirm = input(Fore.BLUE + "Is This Info Correct? (y/n): ")
-if str(ynconfirm.lower()) == "y":
-    continueprcs()
-elif str(ynconfirm.lower()) == "n":
-    print(Fore.RED + "Closing...")
-    sys.exit(1)
+print(Fore.GREEN + "Trying To To Serial Port...")
+try:
+    ser = serial.Serial(comport, baudrate)
+except KeyboardInterrupt:
+    sys.exit(0)
+except serial.SerialException:
+    print(Fore.RED + "Details Are Wrong, Please Check!" + Style.RESET_ALL)
+except:
+    print(Fore.RED + "Something Has Gone Really Wrong!\nClosing Program..." + Style.RESET_ALL)
+    sys.exit(2)
+print(Fore.GREEN + "Connected To Serial Port!" + Style.RESET_ALL)
+datatobesent = input(Fore.BLUE + "What Do You Want To Send?: ")
+if len(str(datatobesent)) == 0:
+    print(Fore.RED + "Data Can't Be Empty!" + Style.RESET_ALL)
+    exit(1)
