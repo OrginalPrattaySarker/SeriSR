30b19c5159bb5b801ebd3dfb7f6d6f6bb37605fc
OpenPrattay <orginalprattaysarkar@gmail.com>
2023-06-22 15:59:28 +0530

    Unnecessary Func Removed

--- main.py
+++ main.py
@@ -11,25 +11,6 @@ import sys
 import colorama
 from colorama import Fore, Back, Style
 
-
-def continueprcs():
-    print(Fore.BLUE + "Connecting To Serial Port...")
-    try:
-        ser = serial.Serial(comport, baudrate)
-    except KeyboardInterrupt:
-        sys.exit(0)
-    except serial.SerialException:
-        print(Fore.RED + "Details Are Wrong, Please Check!" + Style.RESET_ALL)
-    except:
-        print(Fore.RED + "Something Has Gone Really Wrong!\nClosing Program..." + Style.RESET_ALL)
-        sys.exit(2)
-    print(Fore.GREEN + "Connected To Serial Port!" + Style.RESET_ALL)
-    datatobesent = input(Fore.BLUE + "What Do You Want To Send?: ")
-    if len(str(datatobesent)) == 0:
-        print(Fore.RED + "Data Can't Be Empty!" + Style.RESET_ALL)
-        exit(1)
-
-
 # Check if arguments are missing
 if len(sys.argv) != 3:
     print(Fore.RED + "Usage: python script.py <COM Port> <Baud Rate>" + Style.RESET_ALL)
@@ -60,7 +41,7 @@ except:
     print(Fore.RED + "Something Has Gone Really Wrong!\nClosing Program..." + Style.RESET_ALL)
     sys.exit(2)
 print(Fore.GREEN + "Connected To Serial Port!" + Style.RESET_ALL)
-datatobesent = input(Fore.BLUE + "What Do You Want To Send?: ")
+datatobesent = input(Fore.GREEN + "What Do You Want To Send?: ")
 if len(str(datatobesent)) == 0:
     print(Fore.RED + "Data Can't Be Empty!" + Style.RESET_ALL)
     exit(1)
