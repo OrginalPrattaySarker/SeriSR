fc64d7abc513de90afc706559370f5a7d6455856
OpenPrattay <orginalprattaysarkar@gmail.com>
2023-06-22 16:14:58 +0530

    Color Update - Linux Friendly Colors

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from colorama import Fore, Back, Style
 
 # Check if arguments are missing
 if len(sys.argv) != 3:
-    print(Fore.RED + "Usage: python script.py <COM Port> <Baud Rate>" + Style.RESET_ALL)
+    print(Fore.RED + Style.BRIGHT + "Usage: python script.py <COM Port> <Baud Rate>" + Style.RESET_ALL)
     sys.exit(1)
 
 # Get the arguments
@@ -22,26 +22,27 @@ baudrate = sys.argv[2]
 
 # Check if any argument is empty
 if not comport or not baudrate:
-    print(Fore.RED + "Error: Both arguments are required.\nUsage: python script.py <COM Port> <Baud Rate>"
+    print(Fore.RED + Style.BRIGHT + "Error: Both arguments are required.\nUsage: python script.py <COM Port> <Baud Rate>"
           + Style.RESET_ALL)
     sys.exit(1)
 
 # Use the arguments
-print(Fore.GREEN + "COM PORT Given: " + comport)
-print(Fore.GREEN + "BAUD RATE Given: " + baudrate)
-print(Fore.GREEN + "Trying To Connect To Serial Port...")
+print(Fore.GREEN + Style.BRIGHT + "----  SeriSR 1.0 By OpenPrattay  ----")
+print(Style.RESET_ALL + "COM PORT Given: " + comport)
+print(Style.RESET_ALL + "BAUD RATE Given: " + baudrate)
+print(Fore.GREEN + Style.BRIGHT + "Trying To Connect To Serial Port...")
 try:
     ser = serial.Serial(comport, baudrate)
 except KeyboardInterrupt:
     sys.exit(0)
 except serial.SerialException:
-    print(Fore.RED + "Details Are Wrong, Please Check!" + Style.RESET_ALL)
+    print(Fore.RED + Style.BRIGHT + "Details Are Wrong, Please Check!" + Style.RESET_ALL)
     sys.exit()
 except:
-    print(Fore.RED + "Something Has Gone Really Wrong!\nClosing Program..." + Style.RESET_ALL)
+    print(Fore.RED + Style.BRIGHT + "Something Has Gone Really Wrong!\nClosing Program..." + Style.RESET_ALL)
     sys.exit(2)
-print(Fore.GREEN + "Connected To Serial Port!" + Style.RESET_ALL)
-datatobesent = input(Fore.GREEN + "What Do You Want To Send?: ")
+print(Fore.GREEN + Style.BRIGHT + "Connected To Serial Port!" + Style.RESET_ALL)
+datatobesent = input("What Do You Want To Send?: ")
 if len(str(datatobesent)) == 0:
-    print(Fore.RED + "Data Can't Be Empty!" + Style.RESET_ALL)
+    print(Fore.RED + Style.BRIGHT + "Data Can't Be Empty!" + Style.RESET_ALL)
     exit(1)
